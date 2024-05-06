92702d364a6d666c2983d9a0ae2dcf6987221af7
dlkksny <kseniadolgan15@gmail.com>
2024-05-06 23:44:55 +0300

    add mirage server

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,11 +3,36 @@ import {useTranslations} from "use-intl";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "@/app/lib/store";
 import {decrement, increment, incrementByAmount} from "@/app/lib/features/counter/counterSlice";
+import {useEffect, useState} from "react";
+import {makeServer} from "@/api/mirage-server";
+import {StudentAttrs} from "@/app/types/students";
 
 const MainPage = () => {
+    const [user, setUser] = useState<StudentAttrs[]>([]);
     const t = useTranslations()
     const count = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        makeServer();
+        async function fetchStudents() {
+            try {
+                const response = await fetch('/api/students',{
+
+                });
+                if (!response.ok) {
+                    throw new Error('Ошибка загрузки студентов');
+                }
+                const data = await response.json();
+                setUser(data.students);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchStudents();
+    },[])
+    console.log(user);
     return (
         <div>
             <h1>{t('label')}</h1>
@@ -26,6 +51,16 @@ const MainPage = () => {
                 >Increment by 2
                 </button>
             </main>
+            <h1>Студенты</h1>
+            <ul>
+                {user?.map((student) => (
+                    <li key={student.id}>
+                        <p>Имя: {student.name}</p>
+                        <p>Год рождения: {student.birthYear}</p>
+                        <p>IDNP: {student.idnp}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
---
+++ src/app/types/students.ts
@@ -0,0 +1,6 @@
+export interface StudentAttrs {
+    id: number;
+    name: string;
+    birthYear: string;
+    idnp: string;
+}
